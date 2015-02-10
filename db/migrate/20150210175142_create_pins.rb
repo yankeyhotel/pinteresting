e3cf1d050160a52a5e3ff82f21d39f5d30a1fd64
Matt McClard <yankeyhotel@gmail.com>
2015-02-10 15:09:41 -0600

    Add pins/users association

--- db/migrate/20150210175142_create_pins.rb
+++ db/migrate/20150210175142_create_pins.rb
@@ -1,9 +1,8 @@
 class CreatePins < ActiveRecord::Migration
-  def change
-    create_table :pins do |t|
-      t.string :description
-
-      t.timestamps null: false
-    end
-  end
+	def change
+		create_table :pins do |t|
+			t.string :description
+			t.timestamps null: false
+		end
+	end
 end
